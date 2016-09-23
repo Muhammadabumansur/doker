87940caf2de578c3fb5c72dc8a192f72252956b9
Magomed Kandaev <muhammad-114@mail.ru>
2016-09-23 18:59:19 +0300

    Make more sections

--- app/js/common.js
+++ app/js/common.js
@@ -3,5 +3,20 @@ $(document).ready(function() {
   $("#owl-example").owlCarousel({
   	items: 1
   });
+
+	 ymaps.ready(init);
+	    var myMap,
+        	    myPlacemark;
+
+	    function init(){     
+	        myMap = new ymaps.Map("map", {
+	            center: [42.96287882, 47.51014142],
+	            zoom: 15
+	        });
+
+	        myPlacemark = new ymaps.Placemark([42.96287882, 47.51014142]);
+
+	        myMap.geoObjects.add(myPlacemark);
+	    }
  
 });
